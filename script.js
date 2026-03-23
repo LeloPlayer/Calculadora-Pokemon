fd767c2f65989a1f90f8852ea885919643d7bb15
Samuel <samueleliasps4@gmail.com>
2026-03-22 22:18:45 -0300

    Mensagens de batalhas e adicionando easter eggs

--- script.js
+++ script.js
@@ -19,3 +19,38 @@ function createBackgroundBalls() {
     bgBalls.appendChild(b);
   }
 }
+
+// ===== POKÉMON BATTLE MESSAGES =====
+const messages = {
+  start: [
+    "PROF. OAK: Bem-vindo\nao mundo das\ncalculadoras!",
+    "PIKACHU usou\nCALCULATION!\nÉ super eficaz!",
+    "POKÉCALC v1.0\npronto para\na batalha!",
+  ],
+  result: [
+    "CRÍTICO!\nResultado obtido!",
+    "É SUPER EFICAZ!",
+    "POKÉMON inimigo\nfoi derrotado!",
+    "PIKACHU ganhou\n{val} EXP!",
+    "A CALCULADORA\nusou MATH!\n{val}",
+  ],
+  error: [
+    "MISSINGNO:\nerro detectado!",
+    "Não é muito\neficaz...",
+    "TEAM ROCKET\nsabotou o cálculo!",
+  ],
+  clear: [
+    "Memória apagada!",
+    "PIKACHU voltou\nà Pokébola!",
+  ],
+  zero: [
+    "SNORLAX bloqueia\no caminho!",
+    "Divisão por zero?\nNem o MEWTWO\nconsegue!",
+  ],
+  special: [
+    "42 é a resposta\npara tudo!",
+    "1337 - Leet\nTrainer mode!",
+    "404 - Pokémon\nnot found!",
+    "Você encontrou\num Pokémon raro!",
+  ],
+};
